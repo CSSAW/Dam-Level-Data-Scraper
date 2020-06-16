8242be3e35410313216eb99a2751dce82f21616a
Lake Yin <la11010e@gmail.com>
2020-06-16 16:18:42 -0400

    Added tool to pull table data

--- scraper_helpers.py
+++ scraper_helpers.py
@@ -31,5 +31,31 @@ def getArchivedURLs(province, daysInterval=7):
     
     return sorted(urls)
 
+def getTableData(url):
+    soup = BeautifulSoup(requests.get(url).text)
+    table = soup.find(findTableHTML)
+    rows = table.findAll(lambda tag: tag.name=='tr') # get every row with the tag 'tr'
+
+    cleanTable = list()
+
+    for row in rows:
+        cleanRow = list()
+        for data in row.find_all("td"):
+            data = data.get_text(strip=True)
+            if data != "Photo" and data != "Indicators": # these parts are useless to us
+                cleanRow.append(data)
+
+        if "Total" not in cleanRow and "Last Week" not in cleanRow and len(cleanRow) > 0: # exclude lines we don't want
+            cleanTable.append(cleanRow)
+
+    return cleanTable
+
+def findTableHTML(tag):
+    # filter for the table of data by looking through tags
+    # gets a table that has a header with "Dam" but doesn't include extraneous stuff
+    return tag.name == "table" and tag.find(lambda header: (header.name == "th" or header.name == "h3") and header.text == "Dam") and "Means latest available data" not in tag.text
+
 def getProvinces(fileName):
     return open(fileName, "r").readlines()
+
+print(getTableData("https://web.archive.org/web/20101007170516/http://www.dwa.gov.za/Hydrology/Weekly/ProvinceWeek.aspx?region=M"))
