2c2810f4d1949b9e9d6ea341e01664906c85474a
Lake Yin <la11010e@gmail.com>
2020-08-08 20:14:02 -0400

    Revert "New visualization options"
    This reverts commit bf062aaf056e0b74403ac4f314103cfb94a9a9e8.

--- visualize.py
+++ visualize.py
@@ -2,20 +2,10 @@ import plotly.express as px
 import pandas as pd
 
 def visualize_diff(df, date):
-    n_df = df.loc[df["Date"] == date & df["Latitude"] != 0]
+    n_df = df.loc[df["Date"] == date]
     n_df["diff"] = n_df["This_Week"] - n_df["Last_Week"]
 
-    fig = px.scatter_mapbox(n_df, lat="Latitude", lon="Longitude", color="diff", size="FSC",
-                  color_continuous_scale=px.colors.cyclical.IceFire, zoom=4, mapbox_style="carto-positron")
+    fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="diff", size="FSC",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
+                  mapbox_style="carto-positron")
     fig.show()
-
-def visualize_diff_timeline(df):
-    n_df = df.loc[df["Latitude"] != 0]
-    n_df["diff"] = n_df["This_Week"] - n_df["Last_Week"]
-
-    fig = px.scatter_mapbox(n_df, lat="Latitude", lon="Longitude", color="diff", size="FSC", 
-                  animation_frame="Date", color_continuous_scale=px.colors.cyclical.IceFire, 
-                  zoom=4, mapbox_style="carto-positron")
-    fig.show()
-
-visualize_diff_timeline(pd.read_csv(r"C:\Users\yinl3\Documents\Programming\CSCI-2960-RCOS\CSSAW\Dam-Level-Data-Scraper\dam_data\normalized\norm_all_dam_levels.csv", sep=','))
